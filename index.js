488f58583232ed87a4949141b3c2fb8ada56a598
Kucenko Pavel <smartflaber@gmail.com>
2021-08-27 20:35:14 +0300

    ready work

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-const cv = document.querySelector('#dop');
-cv.innerHTML = ('sdfd')
-cv.style.color = 'green'
+// const cv = document.querySelector('#dop');
+// cv.innerHTML = ('sdfd')
+// cv.style.color = 'green'
 
-const rr = document.querySelector('#dop2')
-rr.innerHTML = (`<ul>
-<li class="er"></li>
-<li class="er"></li>
-<li class="er"></li>
-</ul>`)
+// const rr = document.querySelector('#dop2')
+// rr.innerHTML = (`<ul>
+// <li class="er"></li>
+// <li class="er"></li>
+// <li class="er"></li>
+// </ul>`)
 
-const cvv = document.querySelectorAll('.er');
-for (let i = 0; i < cvv.length; i++) {
-  cvv[i].innerHTML = ('sdfdsfdsfd')
-  cvv[i].style.fontStyle = 'italic'
+// const cvv = document.querySelectorAll('.er');
+// for (let i = 0; i < cvv.length; i++) {
+//   cvv[i].innerHTML = ('sdfdsfdsfd')
+//   cvv[i].style.fontStyle = 'italic'
+// }
+
+//создание элементов через js
+
+const newSpan = document.createElement('span');
+document.body.appendChild(newSpan)
+newSpan.innerHTML = 'Привет!';
+newSpan.classList.add('value')
+// newSpan.style.fontStyle = 'italic'
+
+
+for (let i = 0; i < 3; i++) {
+  const newDiv = document.createElement('div')
+newDiv.id = 'testqq';
+newDiv.textContent = 'asd'
+document.body.appendChild(newDiv);
+document.body.appendChild(newDiv);
+document.body.appendChild(newDiv);
 }
-
